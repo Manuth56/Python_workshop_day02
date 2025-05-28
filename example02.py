81f603443afaca5f2f1d1d821c35f9b8edbb1774
Manuth56 <manuthtwin1@gmail.com>
2025-05-28 18:52:15 +0530

    Add files via upload

---
+++ example02.py
@@ -0,0 +1,31 @@
+#def BirthdayWish(name):
+ #   print("Happy Birthday",name)
+ #   print("You are now old")
+#BirthdayWish("Manuth")
+#BirthdayWish("Fahath")
+
+#def BirthdayWish(name,age):
+ #   print(f"Happy Birthday {name}")
+  #  print("You are now old")
+
+#BirthdayWish("Manuth",21)
+#BirthdayWish("Fahath",21)
+
+
+def large(a,b):
+    if(a>b):
+        print("Maximum number is ",a)
+    print("Maximum number is",b)
+
+a=int(input( "Enter number 1:"))
+b=int(input( "Enter number 2:"))
+
+large (a,b)
+
+
+#num3=int(input( "Enter number 1"))
+#num4=int(input( "Enter number 1"))
+
+
+
+
